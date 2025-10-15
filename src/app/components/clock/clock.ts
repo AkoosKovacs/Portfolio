86bb67f0845c67fc725d6d3e882856828d7ccda2
AkoosKovacs <kovacsakoos@gmail.com>
2025-10-16 01:02:27 +0200

    Main page-responsive

---
+++ src/app/components/clock/clock.ts
@@ -0,0 +1,99 @@
+import { Component, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
+
+@Component({
+  selector: 'app-clock',
+  templateUrl: './clock.html',
+  styleUrls: ['./clock.scss'],
+})
+export class ClockComponent implements AfterViewInit {
+  @ViewChild('canvas1') canvas!: ElementRef<HTMLCanvasElement>;
+  ctx!: CanvasRenderingContext2D;
+  radius!: number;
+
+  ngAfterViewInit(): void {
+    this.ctx = this.canvas.nativeElement.getContext('2d')!;
+    this.radius = this.canvas.nativeElement.height / 2;
+    this.ctx.translate(this.radius, this.radius);
+    this.radius = this.radius * 0.9;
+
+    setInterval(() => this.drawClock(), 1000);
+  }
+
+  drawClock() {
+    this.drawFace();
+    this.drawNumbers();
+    this.drawTime();
+  }
+
+  drawFace() {
+    let ctx = this.ctx;
+    let radius = this.radius;
+
+    ctx.beginPath();
+    ctx.arc(0, 0, radius, 0, 2 * Math.PI);
+    ctx.fillStyle = 'White';
+    ctx.fill();
+
+    ctx.lineWidth = radius * 0.1;
+    ctx.stroke();
+
+    ctx.beginPath();
+    ctx.arc(0, 0, radius * 0.1, 0, 2 * Math.PI);
+    ctx.fillStyle = '#333';
+    ctx.fill();
+  }
+
+  drawNumbers() {
+    let ctx = this.ctx;
+    let radius = this.radius;
+    let ang: number;
+    let num: number;
+
+    ctx.font = radius * 0.15 + 'px arial';
+    ctx.textBaseline = 'middle';
+    ctx.textAlign = 'center';
+
+    for (num = 1; num < 13; num++) {
+      ang = (num * Math.PI) / 6;
+      ctx.rotate(ang);
+      ctx.translate(0, -radius * 0.85);
+      ctx.rotate(-ang);
+      ctx.fillText(num.toString(), 0, 0);
+      ctx.rotate(ang);
+      ctx.translate(0, radius * 0.85);
+      ctx.rotate(-ang);
+    }
+  }
+
+  drawTime() {
+    let ctx = this.ctx;
+    let radius = this.radius;
+
+    const now = new Date();
+    let hour = now.getHours();
+    let minute = now.getMinutes();
+    let second = now.getSeconds();
+
+    hour = hour % 12;
+    hour = (hour * Math.PI) / 6 + (minute * Math.PI) / (6 * 60) + (second * Math.PI) / (360 * 60);
+    this.drawHand(hour, radius * 0.5, radius * 0.07);
+
+    minute = (minute * Math.PI) / 30 + (second * Math.PI) / (30 * 60);
+    this.drawHand(minute, radius * 0.8, radius * 0.07);
+
+    second = (second * Math.PI) / 30;
+    this.drawHand(second, radius * 0.9, radius * 0.02);
+  }
+
+  drawHand(pos: number, length: number, width: number) {
+    let ctx = this.ctx;
+    ctx.beginPath();
+    ctx.lineWidth = width;
+    ctx.lineCap = 'round';
+    ctx.moveTo(0, 0);
+    ctx.rotate(pos);
+    ctx.lineTo(0, -length);
+    ctx.stroke();
+    ctx.rotate(-pos);
+  }
+}
--- src/app/components/office/office.ts
+++ src/app/components/office/office.ts
@@ -1,11 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef, HostListener, AfterViewInit } from '@angular/core';
+import { ClockComponent } from '../clock/clock';
 
 @Component({
   selector: 'app-office',
-  imports: [],
+  standalone: true,
+  imports: [ClockComponent],
   templateUrl: './office.html',
-  styleUrl: './office.scss'
+  styleUrls: ['./office.scss'],
 })
-export class Office {
+export class Office implements AfterViewInit {
+  @ViewChild('officeWrapper') wrapper!: ElementRef;
+  @ViewChild('officeImage') image!: ElementRef;
 
+  ngAfterViewInit() {
+    // Középre állítás mobilon a kép betöltése után
+    this.centerScroll();
+  }
+
+  @HostListener('window:resize')
+  onResize() {
+    // Ablakméret változásakor frissítjük a scroll közép pozícióját
+    this.centerScroll();
+  }
+
+  centerScroll() {
+    const el = this.wrapper.nativeElement;
+    const imgEl = this.image.nativeElement;
+
+    if (window.innerWidth < 1200) {
+      // csak mobil nézet
+      const scrollableWidth = imgEl.clientWidth - el.clientWidth;
+      if (scrollableWidth > 0) {
+        // kép közepéről indul a scroll
+        el.scrollLeft = scrollableWidth / 2;
+      }
+    }
+  }
+
+  openMenu(menu: string) {
+    // Itt lehet majd kezelni a hotspotok kattintását
+    console.log('Megnyitva:', menu);
+  }
 }
